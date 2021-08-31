69626767cc5fb924bfdd0dd30b5917989a6e9d19
Emil <emil.swiniarski7@gmail.com>
2021-08-31 21:49:45 +0200

    cleaning

--- swagger.sh
+++ swagger.sh
@@ -1,5 +1,3 @@
 #/bin/bash
 
 docker run --rm -it -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger "$@"
-
-# docker pull quay.io/goswagger/swagger
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd):/abc -w /abc yt-dl:1.0 youtube-dl "$@"
+docker run --rm -v $(pwd):/abc -w /abc yt-dl:2.0 youtube-dl "$@"
 
 #youtube-dl.sh --extract-audio --audio-format mp3 [yt-video-url]
